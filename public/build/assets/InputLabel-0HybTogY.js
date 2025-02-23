540ac956e7d41f15d58190aad4ad3106baf64979
Aurelie05 <roxaneakredjoro@gmail.com>
2025-02-23 18:21:54 +0000

    welcomeTT

---
+++ public/build/assets/InputLabel-0HybTogY.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-DVk2vNyw.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
